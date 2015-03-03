dfe0b71bb01f7dcac64c9ca52bd20b04b39e4156
Michael Iovine <mike.iovine7@gmail.com>
2015-03-03 07:28:53 -0800

    Decoder now brute forces Y value.
    TO DO:
    -Make clean
    -Cut out the use of convert.py (Define a function to call in encode.py)

--- Binary-String-Steganography/decode.py
+++ Binary-String-Steganography/decode.py
@@ -6,44 +6,49 @@ rawim = raw_input('Enter a file to decode: ')
 im = Image.open(rawim)
 pixel = im.load()
 pix_x, pix_y = im.size
-y = int(raw_input('Enter Y value of message: '))
 string = []
-for x in range(pix_x):
-	values = len(im.getpixel((x,y)))
-	if values == 4:
-		r,g,b,a = im.getpixel((x,y))
-		if r % 2 == 0:
-			string.append('0')
-		if r % 2 == 1:
-			string.append('1')
-		if g % 2 == 0:
-			string.append('0')
-		if g % 2 == 1:
-			string.append('1')
-		if b % 2 == 0:
-			string.append('0')
-		if b % 2 == 1:
-			string.append('1')
-		if a % 2 == 1:
-			string.append('1')
-		if a % 2 == 0:
-			string.append('0')
-	elif values == 3:
-		r,g,b = im.getpixel((x,y))
-		if r % 2 == 0:
-			string.append('0')
-		if r % 2 == 1:
-			string.append('1')
-		if g % 2 == 0:
-			string.append('0')
-		if g % 2 == 1:
-			string.append('1')
-		if b % 2 == 0:
-			string.append('0')
-		if b % 2 == 1:
-			string.append('1')
+y = 0
+def decode():
+	for x in range(pix_x):
+		values = len(im.getpixel((x,y)))
+		if values == 4:
+			r,g,b,a = im.getpixel((x,y))
+			if r % 2 == 0:
+				string.append('0')
+			if r % 2 == 1:
+				string.append('1')
+			if g % 2 == 0:
+				string.append('0')
+			if g % 2 == 1:
+				string.append('1')
+			if b % 2 == 0:
+				string.append('0')
+			if b % 2 == 1:
+				string.append('1')
+			if a % 2 == 1:
+				string.append('1')
+			if a % 2 == 0:
+				string.append('0')
+		elif values == 3:
+			r,g,b = im.getpixel((x,y))
+			if r % 2 == 0:
+				string.append('0')
+			if r % 2 == 1:
+				string.append('1')
+			if g % 2 == 0:
+				string.append('0')
+			if g % 2 == 1:
+				string.append('1')
+			if b % 2 == 0:
+				string.append('0')
+			if b % 2 == 1:
+				string.append('1')
+while not '1' in string:
+	y += 1
+	string = []
+	decode()
 while len(string) % 8 != 0:
 	string.append('0')
-binary = ''.join(string)
+binary = ''.join(string) 
 message = ''.join(chr(int(binary[i:i+8], 2)) for i in xrange(0, len(binary), 8))
 print message

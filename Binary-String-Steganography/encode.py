ed0511783d2d9c7453a472f68bae4288d4ec6330
Michael Iovine <mike.iovine7@gmail.com>
2015-03-04 07:13:01 -0800

    Encoder now automatically converts images

--- Binary-String-Steganography/encode.py
+++ Binary-String-Steganography/encode.py
@@ -1,13 +1,39 @@
 #Created by Michael Iovine
 #California Academy of Mathematics and Science 
 from PIL import Image 
-
+rawim = raw_input('Choose a file to encode: ')
+im = Image.open(rawim)
+def convert():
+	pixels = im.load()
+	pix_x, pix_y = im.size
+	for x in range(pix_x):
+		for y in range(pix_y):
+			values = im.getpixel((x,y))
+			if len(values) == 4:
+				r, g, b, a = im.getpixel((x,y))
+				if r % 2 == 1:
+					r = r-1 
+				if g % 2 == 1:
+					g = g-1
+				if b % 2 == 1:
+					b = b-1
+				if a % 2 ==1:
+					a = a-1
+				pixels[x,y] = (r,g,b,a)
+			elif len(values) == 3:
+				r,g,b = im.getpixel((x,y))
+				if r % 2 == 1:
+					r = r-1 
+				if g % 2 == 1:
+					g = g-1
+				if b % 2 == 1:
+					b = b-1
+				pixels[x,y] = (r,g,b)
+convert()
 message = raw_input('Enter a sring to hide: ')
 binary_str = ''.join(format(ord(a),'b').zfill(8) for a in message)
 binary = list(binary_str)
-rawim = raw_input('Select an input file: ')
 y = int(raw_input('Select starting Y value: '))
-im = Image.open(rawim)
 pixel = im.load()
 i = 0
 x = 0
